94b859aa30df60f504b24bd16a15b4144b464db7
Muhamed Agic <muhamedagic@florinco.nl>
2024-10-26 21:03:36 +0200

    day2 20201 part 1

---
+++ src/bin/day2_2021.rs
@@ -0,0 +1,27 @@
+use itertools::Itertools;
+use advent_of_code_2024::utils;
+
+fn part_one(input: &String) -> i32 {
+    let mut depth = 0;
+    let mut horizontal_distance = 0;
+    for line in input.lines() {
+        let (direction, distance) = line.split(' ').collect_tuple().unwrap();
+        let distance = distance.parse::<i32>().unwrap();
+        let _ = match direction {
+            "forward" => horizontal_distance += distance,
+            "backward" => horizontal_distance -= distance,
+            "up" => depth -= distance,
+            "down" => depth += distance,
+            _ => panic!("aahh!"),
+        };
+    }
+    return horizontal_distance * depth;
+}
+
+fn main() {
+    // let input = utils::read_input_from_path("example_input/day2_2021.txt");
+    let input = utils::read_input_from_path("input/day2_2021.txt");
+
+    println!("Part One: {}", part_one(&input));
+    // println!("Part Two: {}", part_two(&input));
+}
