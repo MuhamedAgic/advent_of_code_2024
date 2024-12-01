e4a0609be7fd4f1ce9d3c337a5f9b4e399a96607
Muhamed Agic <muhamedagic@florinco.nl>
2024-12-01 10:04:27 +0100

    part one done

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,13 +1,33 @@
+use itertools::Itertools;
 use advent_of_code_2024::utils;
 
 
-fn part_one(input: &str) -> u32 {
+fn part_one(input: &str) -> i32 {
     // Implement part one solution
-    println!("Test 1");
-    0
+    let mut first_location_ids = Vec::new();
+    let mut second_location_ids = Vec::new();
+
+    for line in input.lines() {
+        let (mut first, mut second): (&str, &str) = line.split("   ").collect_tuple().unwrap();
+        let first = first.parse::<i32>().unwrap();
+        let second = second.parse::<i32>().unwrap();
+        first_location_ids.push(first);
+        second_location_ids.push(second);
+    }
+
+    first_location_ids.sort_unstable();
+    second_location_ids.sort_unstable();
+
+    let mut total_distance = 0;
+    for (first, second) in first_location_ids.iter().zip(second_location_ids.iter()) {
+        let mut distance = second - first;
+        total_distance += distance.abs();
+    }
+
+    total_distance
 }
 
-fn part_two(input: &str) -> u32 {
+fn part_two(input: &str) -> i32 {
     // Implement part two solution
     println!("Test 2");
     0
