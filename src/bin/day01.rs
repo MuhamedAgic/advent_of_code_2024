6f5dd7aedb2d7495062fde554e1295f853200b52
Muhamed Agic <muhamedagic@florinco.nl>
2024-12-01 10:11:46 +0100

    part two done

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,9 +1,7 @@
 use itertools::Itertools;
 use advent_of_code_2024::utils;
 
-
 fn part_one(input: &str) -> i32 {
-    // Implement part one solution
     let mut first_location_ids = Vec::new();
     let mut second_location_ids = Vec::new();
 
@@ -28,9 +26,29 @@ fn part_one(input: &str) -> i32 {
 }
 
 fn part_two(input: &str) -> i32 {
-    // Implement part two solution
-    println!("Test 2");
-    0
+    let mut first_location_ids = Vec::new();
+    let mut second_location_ids = Vec::new();
+
+    for line in input.lines() {
+        let (mut first, mut second): (&str, &str) = line.split("   ").collect_tuple().unwrap();
+        let first = first.parse::<i32>().unwrap();
+        let second = second.parse::<i32>().unwrap();
+        first_location_ids.push(first);
+        second_location_ids.push(second);
+    }
+
+    let mut similarity_score = 0;
+    for first in first_location_ids.iter() {
+        let mut appearances = 0;
+        for second in second_location_ids.iter() {
+            if first == second {
+                appearances += 1;
+            }
+        }
+        similarity_score += first * appearances;
+    }
+
+    similarity_score
 }
 
 fn main() {
